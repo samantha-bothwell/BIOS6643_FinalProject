b235a5b09c4616e6f35eb607a2e0461fd2a69525
Samantha Bothwell <samantha.bothwell@cuanschutz.edu>
2020-10-16 13:18:28 -0600

    Initialize project folders

---
+++ Code/01_Data_Organization.R
@@ -0,0 +1,11 @@
+#####################################################
+####             BIOS 6643 Project               ####
+####        Longitudinal Data Analysis           #### 
+#### Aim : Explain daily weight data through     ####
+####       Functional Data Analysis              ####
+#####################################################
+
+
+#### Load in the data 
+wt <- read.csv("daily_weights_with_confounding.csv")
+
