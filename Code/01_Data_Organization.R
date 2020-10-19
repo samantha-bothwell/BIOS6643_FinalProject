5ac8018c494f67c0de158d4efb5645e022f29d5e
Samantha Bothwell <samantha.bothwell@cuanschutz.edu>
2020-10-19 15:13:29 -0600

    Prelim plots

--- Code/01_Data_Organization.R
+++ Code/01_Data_Organization.R
@@ -7,5 +7,19 @@
 
 
 #### Load in the data 
-wt <- read.csv("daily_weights_with_confounding.csv")
+wt <- read.csv("D:/CU/Fall 2020/BIOS 6643/Project/BIOS6643_FinalProject/DataProcessed/daily_weights_with_confounding.csv")
 
+
+#### Plot data x-axis: study_days, y-axis:  wt_lb, group by: participant_id
+library(ggplot2)
+
+ggplot(data = wt, aes(x = study_days, y = wt_lb, group = participant_id)) + 
+  geom_line()
+
+#### Transform into wide format - use only the weight variable over time 
+library(dplyr)
+library(reshape2)
+library(tidyr)
+wt.wide <- wt %>% 
+  select(wt_lb, study_days) %>% 
+  spread(study_days, wt_lb)
